68182412eba90551e35843a886ad176c03e89864
Alisa Khabibrakhmanova <aakhabibrakhmanova@edu.hse.ru>
2025-01-13 19:32:37 +0300

    first commit

---
+++ backend/src/test/java/com/sinenomine/plottracker/PlottrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinenomine.plottracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlottrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
