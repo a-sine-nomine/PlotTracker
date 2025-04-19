f5b76745a8f3a75d45f1dd0c54bddf7f846d1f5f
a-sine-nomine <alisa.k.tech@gmail.com>
2025-04-19 16:36:43 +0300

    Allow http requests

--- backend/src/main/java/com/sinenomine/plottracker/config/SecurityConfig.java
+++ backend/src/main/java/com/sinenomine/plottracker/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
